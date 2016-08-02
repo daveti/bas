5d855619d0c06c618b323c64d850fba92faf0ee1
daveti <daveti@cs.uoregon.edu>
2016-08-01 22:31:12 -0400

    syscall examples

---
+++ x86/syscalltest2.s
@@ -0,0 +1,24 @@
+# syscalltest2.s - An example of getting a return value from a syscall
+.section .bss
+	.lcomm pid, 4
+	.lcomm uid, 4
+	.lcomm gid, 4
+
+.section .text
+.globl _start
+_start:
+	movl $20, %eax
+	int $0x80
+	movl %eax, pid
+
+	movl $24, %eax
+	int $0x80
+	movl %eax, uid
+
+	movl $47, %eax
+	int $0x80
+	movl %eax, gid
+
+	movl $1, %eax
+	movl $0, %ebx
+	int $0x80
